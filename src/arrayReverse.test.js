dd235f7bc6ef1d0be0a7d68298481df3936be9d5
viktorlyt <viktorlyt@gmail.com>
2023-05-15 14:27:50 +0300

    done

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -8,12 +8,21 @@ describe(`Function 'arrayReverse':`, () => {
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse([])).toBeInstanceOf(Array);
   });
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
     expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  });
+
+  it(`should return an empty string
+    if original array consists of an empty string`, () => {
+    expect(arrayReverse([''])).toEqual(['']);
+  });
+
+  it(`should return an reversed array`, () => {
+    expect(arrayReverse(['I', 'am', 'a', 'student!'])).toEqual(['!', 'tn', 'e', 'dutsamaI']);
   });
 
   // write more tests here
